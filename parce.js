b3d0f2951779d76d0e2c96d127263fb37fe33b79
ivanpopov <ivanpopov140496@gmail.com>
2025-08-31 13:16:41 +0200

    only_js

--- parce.js
+++ parce.js
@@ -1,18 +1,38 @@
 const fs = require('fs');
 
-// Чтение файла
-const rawData = fs.readFileSync('data.json', 'utf8');
-const products = JSON.parse(rawData);
+async function get_html(url) {
+    const response = await fetch(url);
+    const html = await response.text();
+    // fs.writeFileSync('index.html', html, 'utf-8');
+    return html
+};
 
-const result = [];
-for (const product of products) {
-    const price = parseFloat(product.parameters[0].cost);
-        result.push({
-            title: product.name,
-            price: price
-        });
+async function get_state_json(html) {
+    const match = html.match(/<script[^>]*id=["']vike_pageContext["'][^>]*>([\s\S]*?)<\/script>/);
+    const state_json_string = match[1].trim();
+    const jsonObj = JSON.parse(state_json_string);
+    const prettyJson = JSON.stringify(jsonObj, null, 2);
+    // fs.writeFileSync('state.json', prettyJson, 'utf-8');
+
+    return jsonObj;
+};
+
+async function parce_json(state_json) {
+    const menu = state_json.initialStoreState.products.all.map(product => { 
+        return {
+            name: product.name,
+            price: product.parameters[0].cost
+        };
+    });
+    const menuJsonString = JSON.stringify(menu, null, 2);
+    fs.writeFileSync('menu.json', menuJsonString, 'utf-8');
+    return menuJsonString;
+};
+
+async function run() {
+    html = await get_html("https://djari.ru/");
+    state_json = await get_state_json(html);
+    menu = await parce_json(state_json);
 }
 
-// Запись результата
-fs.writeFileSync('products.json', JSON.stringify(result, null, 2), 'utf8');
-console.log('Список сохранен в products.json');
+run();
--- run.sh
+++
@@ -1,2 +0,0 @@
-curl -s "https://djari.ru" | grep 'vike_pageContext' | sed 's/.*<script[^>]*>//' | sed 's/<\/script>.*//' | jq '.initialStoreState.products.all' > data.json
-node parce.js
